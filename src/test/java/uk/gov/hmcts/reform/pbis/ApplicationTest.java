a47ffb88bf6f5b24a7e47bf1cfc85b0a92cee91b
leszekg <gonczar@gmail.com>
2017-11-16 10:50:46 +0000

    Make sure application unit test doesn't do any real work

--- src/test/java/uk/gov/hmcts/reform/pbis/ApplicationTest.java
+++ src/test/java/uk/gov/hmcts/reform/pbis/ApplicationTest.java
@@ -3,9 +3,18 @@ package uk.gov.hmcts.reform.pbis;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource(properties =
+    {
+        // prevent the application from doing actual work when context loads
+        "scheduling.enable=false",
+        "serviceBus.useStub=true",
+        "notify.useStub=true"
+    }
+)
 @SpringBootTest
 public class ApplicationTest {
 
