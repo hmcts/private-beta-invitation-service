55a5c78713c46d1316439c2b9531fada6cd541ef
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-03-22 11:06:35 +0000

    Bump java-logging version to 2.0.2

--- src/test/java/uk/gov/hmcts/reform/pbis/ApplicationTest.java
+++ src/test/java/uk/gov/hmcts/reform/pbis/ApplicationTest.java
@@ -1,9 +1,6 @@
 package uk.gov.hmcts.reform.pbis;
 
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.EnvironmentVariables;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -20,14 +17,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 )
 @SpringBootTest
 public class ApplicationTest {
-
-    @ClassRule
-    public static EnvironmentVariables variables = new EnvironmentVariables();
-
-    @BeforeClass
-    public static void setUp() {
-        variables.set("APPLICATION_INSIGHTS_IKEY", "some-key");
-    }
 
     @Test
     public void contextLoads() {
