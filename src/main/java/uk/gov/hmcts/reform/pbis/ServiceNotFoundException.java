324e691db094489cd7ca2d233fa958d4dae8d4ac
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-03-22 11:13:19 +0000

    Replace parent calss to helper class provided by library

--- src/main/java/uk/gov/hmcts/reform/pbis/ServiceNotFoundException.java
+++ src/main/java/uk/gov/hmcts/reform/pbis/ServiceNotFoundException.java
@@ -1,8 +1,15 @@
 package uk.gov.hmcts.reform.pbis;
 
-public class ServiceNotFoundException extends RuntimeException {
+import uk.gov.hmcts.reform.logging.exception.AlertLevel;
+import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
+
+/**
+ * SonarQube reports as error. Max allowed - 5 parents
+ */
+@SuppressWarnings("squid:MaximumInheritanceDepth")
+public class ServiceNotFoundException extends UnknownErrorCodeException {
 
     public ServiceNotFoundException(String message) {
-        super(message);
+        super(AlertLevel.P3, message);
     }
 }
--- src/main/java/uk/gov/hmcts/reform/pbis/servicebus/ServiceBusException.java
+++ src/main/java/uk/gov/hmcts/reform/pbis/servicebus/ServiceBusException.java
@@ -1,8 +1,15 @@
 package uk.gov.hmcts.reform.pbis.servicebus;
 
-public class ServiceBusException extends RuntimeException {
+import uk.gov.hmcts.reform.logging.exception.AlertLevel;
+import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
+
+/**
+ * SonarQube reports as error. Max allowed - 5 parents
+ */
+@SuppressWarnings("squid:MaximumInheritanceDepth")
+public class ServiceBusException extends UnknownErrorCodeException {
 
     public ServiceBusException(String message, Throwable cause) {
-        super(message, cause);
+        super(AlertLevel.P1, message, cause);
     }
 }
