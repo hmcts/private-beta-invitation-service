8942ebbac96d334678f9084de622c173dd45cd18
donatas M <doncem@users.noreply.github.com>
2018-04-05 08:49:50 +0100

    Update java logging (#42)
    * Bump java-logging version to 2.0.2
    
    * Add service implementation version
    
    * Replace parent calss to helper class provided by library
    
    * Add missed environment variable

--- src/main/java/uk/gov/hmcts/reform/pbis/EmailSendingException.java
+++ src/main/java/uk/gov/hmcts/reform/pbis/EmailSendingException.java
@@ -1,8 +1,15 @@
 package uk.gov.hmcts.reform.pbis;
 
-public class EmailSendingException extends RuntimeException {
+import uk.gov.hmcts.reform.logging.exception.AlertLevel;
+import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
+
+/**
+ * SonarQube reports as error. Max allowed - 5 parents
+ */
+@SuppressWarnings("squid:MaximumInheritanceDepth")
+public class EmailSendingException extends UnknownErrorCodeException {
 
     public EmailSendingException(String message, Throwable cause) {
-        super(message, cause);
+        super(AlertLevel.P1, message, cause);
     }
 }
