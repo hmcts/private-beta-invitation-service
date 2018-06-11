8c24a35955fd2bf78a7957876b9c5182ae0c5b84
Rafal Kondratowicz <rkondr@gmail.com>
2018-06-11 14:56:02 +0200

    Add missing 'subscription' terraform field

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,7 @@ module "service" {
   location = "${var.location}"
   env      = "${var.env}"
   ilbIp    = "${var.ilbIp}"
+  subscription  = "${var.subscription}"
 
   app_settings = {
     SERVICE_BUS_POLLING_DELAY_MS = "${var.service_bus_polling_delay_ms}"
