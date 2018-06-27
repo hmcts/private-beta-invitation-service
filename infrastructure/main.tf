b58a26b33b63175fc4e6d46a9a0ffb8c36aef8bc
Rafal Kondratowicz <rkondr@gmail.com>
2018-06-27 16:50:35 +0100

    Fix resource group for vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,11 +6,20 @@ locals {
   preview_vault_name     = "${var.raw_product}-aat"
   default_vault_name     = "${var.raw_product}-${var.env}"
   vault_name             = "${(var.env == "preview" || var.env == "spreview") ? local.preview_vault_name : local.default_vault_name}"
+
+  preview_vault_rg       = "${var.raw_product}-aat"
+  default_vault_rg       = "${var.raw_product}-${var.env}"
+  vault_rg               = "${(var.env == "preview" || var.env == "spreview") ? local.preview_vault_rg : local.default_vault_rg}"
 }
 
 # Make sure the resource group exists
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
+  name     = "${var.product}-${var.env}"
+  location = "${var.location}"
+}
+
+resource "azurerm_resource_group" "vault_rg" {
+  name     = "${local.preview_vault_rg}"
   location = "${var.location}"
 }
 
@@ -62,7 +71,7 @@ module "key-vault" {
   env                 = "${var.env}"
   tenant_id           = "${var.tenant_id}"
   object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = "${azurerm_resource_group.vault_rg.name}"
   # dcd_cc-dev group object ID
   product_group_object_id = "38f9dea6-e861-4a50-9e73-21e64f563537"
 }
