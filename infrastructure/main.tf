a1a107543130705fe6497e76b8f63edc8baf89fe
luigibk <41112628+luigibk@users.noreply.github.com>
2018-11-16 14:25:31 +0000

    Add missing tags (#71)
    * Add tags
    
    * Add missing var
    
    * Fix missing sonar properties

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,6 +30,8 @@ module "servicebus-namespace" {
   name                  = "${var.product}-servicebus-${var.env}"
   location              = "${var.location}"
   resource_group_name   = "${azurerm_resource_group.rg.name}"
+  env                   = "${var.env}"
+  common_tags           = "${var.common_tags}"
 }
 
 module "servicebus-topic" {
