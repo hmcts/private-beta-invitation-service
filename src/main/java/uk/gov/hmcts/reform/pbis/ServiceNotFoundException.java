e8dc3d8d35b5658a885ac157424bcd2b057341ee
Kevlar <kevin.j.cross@gmail.com>
2019-01-29 09:24:58 +0000

    updating to spring 2.10.2

--- src/main/java/uk/gov/hmcts/reform/pbis/ServiceNotFoundException.java
+++ src/main/java/uk/gov/hmcts/reform/pbis/ServiceNotFoundException.java
@@ -1,15 +1,7 @@
 package uk.gov.hmcts.reform.pbis;
 
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
-
-/**
- * SonarQube reports as error. Max allowed - 5 parents
- */
-@SuppressWarnings("squid:MaximumInheritanceDepth")
-public class ServiceNotFoundException extends UnknownErrorCodeException {
-
+public class ServiceNotFoundException extends RuntimeException {
     public ServiceNotFoundException(String message) {
-        super(AlertLevel.P3, message);
+        super(message);
     }
 }
