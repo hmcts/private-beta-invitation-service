48940f27ffb05ce1ab30a43d660592b7717e6eee
Kevlar <kevin.j.cross@gmail.com>
2019-01-30 15:23:39 +0000

    removing the integration dependency

--- src/test/java/uk/gov/hmcts/reform/pbis/EmailServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pbis/EmailServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.pbis.notify.EmailToSend;
 import uk.gov.hmcts.reform.pbis.notify.NotificationClientProvider;
 import uk.gov.hmcts.reform.pbis.servicebus.PrivateBetaRegistration;
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class EmailServiceTest {
 
     private EmailService emailService;
