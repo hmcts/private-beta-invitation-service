9627547fd7900829bf2f4441a06b8d7e85c1ac4c
Tim Jacomb <tim.jacomb@hmcts.net>
2019-07-16 10:43:57 +0100

    Fix stub

--- src/main/java/uk/gov/hmcts/reform/pbis/notify/NotificationClientStub.java
+++ src/main/java/uk/gov/hmcts/reform/pbis/notify/NotificationClientStub.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.pbis.notify;
 
+import java.io.File;
+import java.io.InputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.gov.service.notify.LetterResponse;
 import uk.gov.service.notify.Notification;
 import uk.gov.service.notify.NotificationClientApi;
 import uk.gov.service.notify.NotificationClientException;
 import uk.gov.service.notify.NotificationList;
+import uk.gov.service.notify.ReceivedTextMessageList;
 import uk.gov.service.notify.SendEmailResponse;
 import uk.gov.service.notify.SendLetterResponse;
 import uk.gov.service.notify.SendSmsResponse;
@@ -23,12 +27,23 @@ public class NotificationClientStub implements NotificationClientApi {
         "Notification client stub doesn't support this method";
 
     @Override
+    public SendEmailResponse sendEmail(String templateId, String emailAddress, Map<String, ?> personalisation, String reference) {
+        logger.info(
+            String.format(
+                "Sending email to %s. Template ID: %s, reference: %s",
+                emailAddress,
+                templateId,
+                reference
+            )
+        );
+
+        return null;
+    }
+
+    @Override
     public SendEmailResponse sendEmail(
-        String templateId,
-        String emailAddress,
-        Map<String, String> personalisation,
-        String reference
-    ) throws NotificationClientException {
+        String templateId, String emailAddress, Map<String, ?> personalisation, String reference, String emailReplyToId
+    ) {
         logger.info(
             String.format(
                 "Sending email to %s. Template ID: %s, reference: %s",
@@ -43,27 +58,45 @@ public class NotificationClientStub implements NotificationClientApi {
 
     @Override
     public SendSmsResponse sendSms(
-        String templateId,
-        String phoneNumber,
-        Map<String, String> personalisation,
-        String reference
-    ) throws NotificationClientException {
+        String templateId, String phoneNumber, Map<String, ?> personalisation, String reference
+    ) {
         throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
     }
 
     @Override
-    public SendLetterResponse sendLetter(
-        String templateId,
-        Map<String, String> personalisation,
-        String reference
-    ) throws NotificationClientException {
+    public SendSmsResponse sendSms(
+        String templateId, String phoneNumber, Map<String, ?> personalisation, String reference, String smsSenderId
+    ) {
         throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
     }
 
     @Override
-    public Notification getNotificationById(
-        String notificationId
-    ) throws NotificationClientException {
+    public SendLetterResponse sendLetter(String templateId, Map<String, ?> personalisation, String reference) {
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
+    }
+
+    @Override
+    public LetterResponse sendPrecompiledLetter(String reference, File precompiledPDF) {
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
+    }
+
+    @Override
+    public LetterResponse sendPrecompiledLetter(String reference, File precompiledPDF, String postage) {
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
+    }
+
+    @Override
+    public LetterResponse sendPrecompiledLetterWithInputStream(String reference, InputStream stream) {
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
+    }
+
+    @Override
+    public LetterResponse sendPrecompiledLetterWithInputStream(String reference, InputStream stream, String postage) {
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
+    }
+
+    @Override
+    public Notification getNotificationById(String notificationId) {
         throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
     }
 
@@ -73,32 +106,32 @@ public class NotificationClientStub implements NotificationClientApi {
         String notificationType,
         String reference,
         String olderThanId
-    ) throws NotificationClientException {
+    ) {
         throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
     }
 
     @Override
-    public Template getTemplateById(String templateId) throws NotificationClientException {
+    public Template getTemplateById(String templateId) {
         throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
     }
 
     @Override
-    public Template getTemplateVersion(
-        String templateId, int version
-    ) throws NotificationClientException {
+    public Template getTemplateVersion(String templateId, int version) {
         throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
     }
 
     @Override
-    public TemplateList getAllTemplates(String templateType) throws NotificationClientException {
+    public TemplateList getAllTemplates(String templateType) {
         throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
     }
 
     @Override
-    public TemplatePreview generateTemplatePreview(
-        String templateId,
-        Map<String, String> personalisation
-    ) throws NotificationClientException {
+    public TemplatePreview generateTemplatePreview(String templateId, Map<String, Object> personalisation) {
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
+    }
+
+    @Override
+    public ReceivedTextMessageList getReceivedTextMessages(String olderThanId) {
         throw new UnsupportedOperationException(UNSUPPORTED_OPERATION_MESSAGE);
     }
 }
